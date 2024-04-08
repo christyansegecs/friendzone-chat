b2b26ef5b255a89114209d5f97b1536fc5845225
christyansegecs <christyansegecs@hotmail.com>
2024-04-08 05:04:05 -0300

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+        'localhost',
+        'lh3.googleusercontent.com',
+    ]
+}
+};
+
+export default nextConfig;
